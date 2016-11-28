3bf2bafe1523a6862950c5d4d921597e6f559bb2
Tina Quach <quacht@mit.edu>
2016-11-28 00:54:06 -0500

    add manifest files for downloading SNP data for brain, breast, kidney, and lung cancer; bash script for unzipping and saving files as txt files for easy reading

---
+++ scripts/unzip.sh
@@ -0,0 +1,33 @@
+# CD into the parent directory containing the folders 
+# organizing the data
+
+# Assumes that the data folder 
+cd ..
+cd ..
+cd gdc_data
+
+for sampleDir in *; do
+	echo $sampleDir
+ 	cd $sampleDir
+ 	for subdir in *; do
+ 		if [ -d "$subdir" ]; then
+	 	 	echo $subdir
+	 	 	cd $subdir
+	 		for file in *; do
+	 			if [[ $file == *.gz ]]; then
+	 				echo "UNZIPPING FILE"
+	 				echo $file
+	 				gzip -d $file
+	 			fi
+	 			if [[ $file == *.maf ]]; then
+	 				echo "SAVING *.maf AS *.maf.txt"
+	 				echo $file
+	 				mv $file "$file.txt"
+	 			fi
+	 		done
+	 		cd ..
+	 	fi
+ 	done
+ 	echo "---"
+ 	cd ..
+done
