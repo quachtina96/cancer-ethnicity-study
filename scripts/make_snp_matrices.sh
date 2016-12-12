99b5bf4c8e1ebd6a9695377d6d159f3ba1b55cf1
Tina Quach <quacht@mit.edu>
2016-12-12 17:58:50 -0500

    make_snp_matrices.sh

---
+++ scripts/make_snp_matrices.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# Make SNP Matrices
+
+echo 'BRCA'
+python snp.py /home/cancer-ethnicity-study/data/gdc_data/BRCA
+echo 'LGG'
+python snp.py /home/cancer-ethnicity-study/data/gdc_data/BRAIN/LGG
+echo 'GBM'
+python snp.py /home/cancer-ethnicity-study/data/gdc_data/BRAIN/GBM
+echo 'LUAD'
+python snp.py /home/cancer-ethnicity-study/data/gdc_data/LUNG/LUAD
+echo 'LUSC'
+python snp.py /home/cancer-ethnicity-study/data/gdc_data/LUNG/LUSC
+
